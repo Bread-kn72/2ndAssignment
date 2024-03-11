1bc9c0d7e62d17493d0e8c4aec4fe1a9a53ede6b
Bread-kn72 <ordinarytom72@gmail.com>
2024-03-11 19:30:13 +0900

    레벨1 세자리 숫자 무작위 생성 구현

--- 2ndAssignment/main.swift
+++ 2ndAssignment/main.swift
@@ -1,11 +1,12 @@
-//
-//  main.swift
-//  2ndAssignment
-//
-//  Created by Kinam on 3/11/24.
-//
 
 import Foundation
 
-print("Hello, World!")
+// LV.1 세자리 임의의 숫자 만들기
+func createNumber() -> Int {
+    var result : Int = 0
+    for i in 0..<3 {
+        result = result * 10 + Int.random(in: 0...9)
+    }
+    return result
+}
 
