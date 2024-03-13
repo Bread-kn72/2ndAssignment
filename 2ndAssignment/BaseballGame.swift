4d4b818f8a2eb90f6ae3584ef87ee9ee5f9d7401
Bread-kn72 <ordinarytom72@gmail.com>
2024-03-13 14:48:22 +0900

    LV.2 구현완료

--- 2ndAssignment/BaseballGame.swift
+++ 2ndAssignment/BaseballGame.swift
@@ -1,18 +1,67 @@
 import Foundation
 
 // BaseballGame.swift 파일 생성
+// BaseballGame 클래스 생성, 게임을 시작하는 함수 생성
 class BaseballGame {
     func start() {
         let answer = makeAnswer() // 정답을 만드는 함수
+        print("<게임을 시작합니다>")
+        // break가 나오기 전까진 무한 반복하는 코드
+        while true {
+            //유저에게 입력값을 받음, 조건 추가로 3자리 숫자 확인
+            print("세 자리 숫자를 입력하세요:")
+            guard let userInput = readLine(), userInput.count == 3 else {
+                print("올바르지 않은 입력값입니다.")
+                continue
+            }
+            // 숫자가 아니거나 중복된 숫자가 있는경우 에러가 나오는 프로퍼티, 조건 추가로 중복된 숫자를 확인하는 함수 추가
+            guard let userNumber = Int(userInput), isValidNumber(userNumber) else {
+                print("잘못된 입력입니다. 숫자가 아니거나, 0을 포함하거나 중복된 숫자가 있습니다.")
+                continue
+            }
+            // 스트라이크와 볼 개수를 프린트하는 상수, 계산하는 함수를 넣어 두가지 값에 strikes, balls 두 값을 반환하도록 설정
+            let (strikes, balls) = compare(answer, userNumber)
+            print("\(strikes)스트라이크, \(balls)볼")
+            
+            // strikes가 3개라면 정답을 프린트 + 브레이크로 while문 마무리
+            if strikes == 3 {
+                print("정답입니다!")
+                break
+            }
+        }
     }
-    
+    // 정답을 만드는 함수, 빈 변수를 하나 만든 뒤 1...9까지 임의의 값을 3번 반복해서 만들어냄 두번째 반복부터는 10이 곱해짐
     func makeAnswer() -> Int {
-        // 함수 내부를 구현하기
-        var result : Int = 0
+        var result = 0
         for _ in 0..<3 {
-            result = result * 10 + Int.random(in: 0...9)
+            result = result * 10 + Int.random(in: 1...9)
+            // 0 * 10 + 8 = 8
+            // 8 * 10 + 9 = 89
+            // 89 * 10 + 1 = 891
         }
         return result
     }
+    // 숫자가 아니거나 중복된 숫자가 있는경우를 확인하는 함수, 셋을 이용하여 중복값이 없도록 참 거짓 구분
+    func isValidNumber(_ number: Int) -> Bool {
+        let digits = Array(String(number))
+        return Set(digits).count == 3
+    }
+    
+    // 숫자를 비교하는 함수, 정답과 추측 수를 배열로 만들어 서로 비교 후 반복문으로 스트라이크와 볼 갯수를 입력 후 반환
+    func compare (_ answer: Int, _ guess: Int) -> (strikes: Int, balls: Int) {
+        let answerDigits = Array(String(answer))
+        let guessDigits = Array(String(guess))
+        
+        var strikes = 0
+        var balls = 0
+        
+        for i in 0..<3 {
+            if guessDigits[i] == answerDigits[i] {
+                strikes += 1
+            } else if answerDigits.contains(guessDigits[i]) {
+                balls += 1
+            }
+        }
+        return (strikes, balls)
+    }
 }
-
--- 2ndAssignment/main.swift
+++ 2ndAssignment/main.swift
@@ -1,6 +1,7 @@
 
 import Foundation
 
+//게임을 실행시키는 인스턴스 생성
 let game = BaseballGame()
 game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
 
